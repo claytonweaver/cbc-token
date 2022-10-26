3791c5d6d2bc95096b17be06002630358a813bdc
Clayton Weaver <cweaver@cbctrenton.com>
2022-10-26 17:05:15 -0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import AWS from 'aws-sdk';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-
-  AWS.config.update({ region: 'us-east-2' });
-
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
