96e42067cbaf4b4af7cf31e075e44f58e8f268a2
Clayton Weaver <cweaver@cbctrenton.com>
2022-10-26 15:57:49 -0400

    more

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import AWS from 'aws-sdk';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  AWS.config.update({ region: 'us-east-2' });
+  AWS.config.
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
--- src/token/token.repository.ts
+++ src/token/token.repository.ts
@@ -1,5 +1,44 @@
+import { AttributeMap, DocumentClient } from "aws-sdk/clients/dynamodb";
+import * as AWS from 'aws-sdk';
+import { InternalServerErrorException, NotFoundException } from "@nestjs/common";
+
 export class TokenRepository {
+    private tableName: string;
+    private db: DocumentClient;
+
+    constructor() {
+        this.tableName = 'TokenTable';
+        this.db = new AWS.DynamoDB.DocumentClient();
+    }
+
     public async getToken(tokenKey: string) {
-        return `token key ${tokenKey}, it works!`;
+        let tokenValue = '';
+
+        try {
+            const request: DocumentClient.GetItemInput = {
+                TableName: this.tableName,
+                Key: {
+                    PK: tokenKey
+                },
+            };
+
+            const result = await this.db.get(request).promise();
+
+            if (result.Item) {
+                tokenValue = result.Item['TokenValue']['S'];
+            }
+
+        } catch (error) {
+            throw new InternalServerErrorException(error, 'Error trying to ' +
+                `connect to table with aws dynamo db client ${this.tableName}` +
+                `with token key ${tokenKey}`
+            );
+        }
+
+        if (!tokenValue) {
+            throw new NotFoundException(`Token value not found for token key ${tokenKey}`)
+        }
+
+        return tokenValue;
     }
 }
